2041e3eccb63be5bfb81b2924a1b2ad662681795
Nightmareterrible <Nightmareterrible@gmail.com>
2022-04-23 14:52:41 +0300

    lesson 2 first commit

---
+++ lesson2/dz1.py
@@ -0,0 +1,3 @@
+first_list = [1, 2, 3, False, [5, 6], "7", '8', 9.10]
+for elem in first_list:
+    print(f"element {elem} have type {type(elem)}")
---
+++ lesson2/dz4.py
@@ -0,0 +1,3 @@
+f = input("enter text : ")
+for i, word in enumerate(f.split()):
+    print(i, ". ", word[0:10])
